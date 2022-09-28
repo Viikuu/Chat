7a09d9a851e0172dd2f010273ba15b706a0d9e3c
Viikuu <viiktorw77@gmail.com>
2022-09-28 12:23:52 +0200

    Code refactor

--- public/src/components/Select.jsx
+++
@@ -1,9 +0,0 @@
-export function Select({options, onChange}) {
-	return (
-		<select onChange={onChange} className={`block p-2 mb-6 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500`}>
-			{options.map(option => {
-				return (<option className={"border-2 border-white hover:bg-violet-900"} key={option.name} value={option.name}> {option.name} </option>)
-			})}
-		</select>
-	)
-}
